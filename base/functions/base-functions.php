a8bc14cbbd12a97537f41eaa2d0bb9cb7f43572f
Pernilla Sterner <pernilla@techlove.se>
2022-01-21 22:08:25 +0100

    Add base functions

---
+++ base/functions/base-functions.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+// Load dependencies
+require_once(get_template_directory() . '/base/functions/inc/custom-functions.php');
+require_once(get_template_directory() . '/base/functions/inc/theme-setup.php');
+// require_once(get_template_directory() . '/base/functions/inc/wysiwyg.php');
+// require_once(get_template_directory() . '/base/functions/inc/gravity-form.php');
+// require_once(get_template_directory() . '/base/functions/inc/customize.php');
+// require_once(get_template_directory() . '/base/functions/inc/dashboard.php');
+// require_once(get_template_directory() . '/base/functions/inc/admin.php');
+// require_once(get_template_directory() . '/base/functions/inc/custom-login-page.php');
+// require_once(get_template_directory() . '/base/functions/inc/widgets/sidebars.php');
+// require_once(get_template_directory() . '/base/functions/inc/global-components.php');
+
+
+// Scripts and styles
+// function awave_base_scripts() {
+//     // JS
+//     //wp_enqueue_script('jquery');
+//     wp_deregister_script( 'jquery' );
+//    wp_register_script( 'jquery', includes_url( '/js/jquery/jquery.js' ), false, NULL, true );
+//     wp_enqueue_script( 'jquery' );
+// }
+// add_action( 'wp_enqueue_scripts', 'awave_base_scripts',1 );
