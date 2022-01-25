cce70eaaf3d7629f1aceee46988ddce02bcc34f4
Pernilla Sterner <pernilla@techlove.se>
2022-01-25 21:06:18 +0100

    First version of hero

---
+++ templates/components/hero.php
@@ -0,0 +1,30 @@
+<?php $hero_content = get_sub_field('content'); ?>
+
+
+<section class="component component_hero big">
+    <?php foreach( $hero_content as $content ) : ?>
+        <?php 
+            $main_image = $content['image'];
+            $header = $content['header'];
+            $header_tag = $content['header_tag'];
+            $preamble = $content['preamble'];
+            $btn_link = $content['button_link'];
+        ?>
+            <div class="hero_image" style="background-image: url('<?php echo (!empty($main_image)) ? $main_image['url'] : ''; ?>');">
+        </div>
+
+        <div class="row hero_content">
+            <div class="col-8">
+                <?php if($header) : ?>
+                    <h2><?php echo $header; ?></h2>
+                <?php endif; ?>
+                <?php if($preamble) : ?>
+                    <p><?php echo $preamble; ?></p>
+                <?php endif; ?>
+            </div>
+            <div class="col-4">
+                <img title="qr-code" alt="qr-code" src="<?php echo get_template_directory_uri() .'/assets/img/qr-code_hero.svg'; ?>">
+            </div>
+        </div>
+    <?php endforeach; ?>
+</section>
