81d928c195441b093e7a048870e25f7f422411db
Pernilla Sterner <pernilla@techlove.se>
2022-01-25 21:56:01 +0100

    Add button to hero section

--- templates/components/hero.php
+++ templates/components/hero.php
@@ -4,14 +4,15 @@
 <section class="component component_hero big">
     <?php foreach( $hero_content as $content ) : ?>
         <?php 
-            $main_image = $content['image'];
-            $header = $content['header'];
-            $header_tag = $content['header_tag'];
-            $preamble = $content['preamble'];
-            $btn_link = $content['button_link'];
+            $main_image     = $content['image'];
+            $header         = $content['header'];
+            $header_tag     = $content['header_tag'];
+            $preamble       = $content['preamble'];
+            $button         = $content['button'];
+            $qr_image       = $content['qr_image'];
         ?>
-            <div class="hero_image" style="background-image: url('<?php echo (!empty($main_image)) ? $main_image['url'] : ''; ?>');">
-        </div>
+        
+        <div class="hero_image" style="background-image: url('<?php echo (!empty($main_image)) ? $main_image['url'] : ''; ?>')"></div>
 
         <div class="row hero_content">
             <div class="col-8">
@@ -21,9 +22,12 @@
                 <?php if($preamble) : ?>
                     <p><?php echo $preamble; ?></p>
                 <?php endif; ?>
+                <?php if($button) : ?>
+                    <a href="<?php $button['link']; ?>" class="btn btn-hero"><?php echo $button['title']; ?></a>
+                <?php endif; ?>
             </div>
             <div class="col-4">
-                <img title="qr-code" alt="qr-code" src="<?php echo get_template_directory_uri() .'/assets/img/qr-code_hero.svg'; ?>">
+                <img title="qr-code" alt="qr-code" src="<?php echo $qr_image['url']; ?>">
             </div>
         </div>
     <?php endforeach; ?>
