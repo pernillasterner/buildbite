60dcde182064e321057cb28940868f181f8ce99e
Pernilla Sterner <pernilla@techlove.se>
2022-01-25 09:51:24 +0100

    Add styling func to footer

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
+// All of the base theme functions
+require_once(get_template_directory() . '/base/functions/base-functions.php');
+// Theme specific functions
+require_once(get_template_directory() . '/inc/custom-functions.php');
+
 
 function buildbite_add_footer_styles() {
     wp_enqueue_style( 'screen', get_template_directory_uri() . '/assets/css/screen.css', array(),'1.0.0', 'screen, print' );
