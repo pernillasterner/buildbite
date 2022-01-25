9cba2b107651a559dca9cae1da9e2775f20501cc
Pernilla Sterner <pernilla@techlove.se>
2022-01-25 15:01:41 +0100

    Add container for navbar

--- templates/nav.php
+++ templates/nav.php
@@ -1,23 +1,21 @@
 <header class="site_header">
 
-    <nav class="top_navigation d-flex justify-content-end">
+    <nav class="top_navigation">
         <?php get_theme_logo(); ?>
 
 
         <div class="menu-toppmeny-container">
-        <?php $args = array(
-            'theme_location'  => 'topmenu',
-            'menu_class'      => 'navbar-nav',
-            'container'         => false
-            // 'fallback_cb'       => false
-        );
+            <?php $args = array(
+                'theme_location'  => 'topmenu',
+                'menu_class'      => 'navbar-nav',
+                'container'       => false,
+                'fallback_cb'     => false
+            );
 
-        wp_nav_menu( $args );
-        
-        ?>
-        <button class="btn get-started-btn">Get started
-            <i class="fas fa-arrow-right"></i>
-        </button>
+            wp_nav_menu( $args );
+            
+            ?>
+            <button class="btn btn-nav">get started</button>
         </div>
     </nav>
 
