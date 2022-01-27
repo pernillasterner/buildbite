b117931b2d67e54071318e51e96f830ebe174cfb
Pernilla Sterner <pernilla@techlove.se>
2022-01-27 18:24:26 +0100

    Add template to text_and_image component

---
+++ templates/components/text_and_image.php
@@ -0,0 +1,39 @@
+<?php 
+$row = get_sub_field('row');
+$header = get_sub_field('header');
+?>
+
+<section class="component component_text_image">
+    <?php if($row) : ?>
+        <div class="row no-gutters pl-3">
+            <h2><?php echo $header; ?></h2>
+            <?php foreach($row as $row) : ?>
+                <?php
+                    $content = $row['content'];
+                    $subheading = $content['subheading'];
+                    $text = $content['text'];
+                    $tags = $content['tags'];
+                    $image = $content['image'];
+                ?>
+                <?php if($subheading) : ?>
+                <div class="row container justify-content-between no-gutters">
+                    <div class="col-6">
+                        <div class="col subheading"><h3><?php echo $subheading; ?></h3></div>
+                        <div class="col subcontent">
+                            <div class="row"><h5><?php echo $text; ?></h5></div>
+                            <?php foreach($tags as $tag) : ?>
+                                <div class="row d-inline-block mb-3 mt-3">
+                                    <ul>
+                                        <li class="mr-1"><h6><?php echo $tag['tag']; ?></h6></li>
+                                    </ul>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 background-col" style="background-image: url(<?php echo $image['url']; ?>);"></div>
+                </div>
+                <?php endif; ?>    
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
+</section>
