6f177a15144f234ebf01a6712c56e57a220f3dc8
Pernilla Sterner <pernilla@techlove.se>
2022-01-28 13:02:20 +0100

    Remove unnecessary code

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php get_header(); ?>
-
-
-<h1>Här är index.php</h1>
-
-
+<?php if(have_posts()) : ?>
+<main id="blog-roll" role="main">
+	<article class="container">
+		<div class="row">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part('templates/post'); ?>
+			<?php endwhile; ?>
+		</div>
+	</article>
+</main><!-- /#main -->
+<?php endif; ?>
 <?php get_footer(); ?>
