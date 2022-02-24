43d1066416aabe84d4573117d6f5f6169e1daff3
Pernilla Sterner <pernilla@techlove.se>
2022-02-24 12:31:41 +0100

    Import moduls. Structure the code

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+import {  MENU } from './modules/menu.js';
+import {  CAROUSEL } from './modules/carousel.js';
+
+
+const APP = (function () {
+    document.addEventListener('DOMContentLoaded', () => {
+
+        const open = document.querySelectorAll('.open');
+        const close = document.querySelectorAll('.modul_close');
+        const modul_get_started = document.getElementById('modul_get-started');
+
+        open.forEach(el => el.addEventListener('click', () => {
+            modul_get_started.classList.add('modul_show')
+        }));
+
+        close.forEach(el => el.addEventListener('click', (e) => {
+            modul_get_started.classList.remove('module_show')
+        }));
+
+        const openFeatures = document.querySelectorAll('.open-features');
+        const modul_features = document.getElementById('modul_features');
+        
+        openFeatures.forEach(el => el.addEventListener('click', () => {
+            modul_features.classList.add('modul_show')
+        }));
+    
+    })
+})();
---
+++ assets/js/modules/menu.js
@@ -0,0 +1,67 @@
+
+const MENU = (function () {
+    // properties
+    const menu_btn = document.querySelector('.navbar_close');
+    const menu_nav = document.querySelector('.main_navigation');
+    const site_header = document.querySelector('.site_header');
+    const sectionOne = document.querySelector('.component_hero');
+    const main_navbar = document.querySelector('.main_logo');
+    const scrolled_navbar = document.querySelector('.scrolled_logo');
+
+    
+    // function addHandler() {
+    //     menu_btn.classList.toggle('is-active');
+    //     menu_nav.classList.toggle('is-active');
+
+    // }
+
+    // function addListeners() {
+    //     menu_btn.addEventListener('click', addHandler)
+    // }
+    
+    const addHandler = () => {
+        menu_btn.classList.toggle('is-active')
+        menu_nav.classList.toggle('is-active')
+    }
+
+    const addListeners = () => {
+        menu_btn.addEventListener('click', addHandler)
+    }
+
+
+
+    const sectionOneOptions = {
+        rootMargin: "-500px 0px 0px 0px"
+    };
+    const sectionOneObserver = new IntersectionObserver(function(
+        entries, 
+        sectionOneOptions
+    ) {
+
+        entries.forEach(entry => {
+
+            main_navbar.style.display = 'block';
+            scrolled_navbar.style.display = 'none';
+           
+            if(!entry.isIntersecting) {
+                site_header.classList.add('bgcolor-scrolled');
+                main_navbar.style.display = 'none';
+                scrolled_navbar.style.display = 'block';
+            } else {
+                site_header.classList.remove('bgcolor-scrolled');
+            }
+        })
+
+    }, 
+    sectionOneOptions);
+
+    sectionOneObserver.observe(sectionOne);
+
+   return { addListeners }
+})();
+
+
+export { MENU }
+
+
+
