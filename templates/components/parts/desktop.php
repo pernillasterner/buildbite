9eda0810e64f72ca2f6677eb697fea4e782141a7
Pernilla Sterner <pernilla@techlove.se>
2022-02-25 13:23:04 +0100

    Small css fixes with paddings

--- templates/components/parts/desktop.php
+++ templates/components/parts/desktop.php
@@ -1,32 +1,32 @@
 <?php
-$row = get_sub_field('row');
+$rows = get_sub_field('row');
 $header = get_sub_field('header');
 ?>
 
-<section class="component component_text_image desktop">
-    <?php if($row) : ?>
+<section id="why_buildbite" class="component component_text_image desktop">
+    <?php if($rows) : ?>
         <div class="row first no-gutters">
             <div class="col left">
                 <h2><?php echo $header; ?></h2>
-                <?php foreach($row as $row) : ?>
+                <?php $y = 0; ?>
+                <?php foreach($rows as $row) : ?>
                 <?php 
                 $content = $row['content'];
                 $subheading = $content['subheading'];
                 $text = $content['text'];
                 $tags = $content['tags'];
-                $image = $content['image'];    
                 ?>
-                <div class="container_content">
-                    <h3 class="col-4"><?php echo $subheading; ?></h3>
-                    <div class="colr-8 container_text">
+                <div id="text_image_container" class="container_content text_image_container<?php echo '-' . $y; ?>">
+                    <h3 id="text_image_subheading" class="col-4 text_image_subheading<?php echo '-' . $y; ?>"><?php echo $subheading; ?></h3>
+                    <div class="col-8 container_text">
                         <h5><?php echo $text; ?></h5>
-                        <?php //foreach($tags as $tag) : ?>
+                        <?php foreach($tags as $tag) : ?>
                             <!-- <div class="row d-inline-block mb-1 mt-1">
                                 <ul>
                                     <li class="mr-1"><h6><?php //echo $tag['tag']; ?></h6></li>
                                 </ul> -->
                             <!-- </div> -->
-                        <?php //endforeach; ?>
+                        <?php endforeach; ?>
                         <?php for($i = 0; $i < 3; $i++) : ?>
                             <div class="row d-inline-block mb-1 mt-2">
                                 <ul>
@@ -36,9 +36,20 @@ $header = get_sub_field('header');
                         <?php endfor; ?>
                     </div>
                 </div>
+                <?php $y++; ?>
                 <?php endforeach; ?>
             </div>
-            <div class="col right background-col" style="background-image: url(<?php echo $image['url']; ?>);"></div>
+            <div id="image-container" class="col right position-relative">
+            <?php $i = 0; ?>
+            <?php foreach($rows as $row) : ?>
+                <?php 
+                $content = $row['content'];
+                $image = $content['image']; 
+                ?>
+                 <div id="background-img<?php echo '-' . $i; ?>" class="background-col" style="background-image: url(<?php echo $image['url']; ?>);"></div>
+                <?php $i++; ?>
+            <?php endforeach; ?>
+            </div>
         </div>
     <?php endif; ?>
 </section>
--- templates/components/parts/tablet.php
+++ templates/components/parts/tablet.php
@@ -3,7 +3,7 @@ $row = get_sub_field('row');
 $header = get_sub_field('header');
 ?>
 
-<section class="component component_text_image tablet">
+<section id="why_buildbite" class="component component_text_image tablet">
     <?php if($row) : ?>
         <div class="row no-gutters pl-3">
             <h2><?php echo $header; ?></h2>
@@ -20,21 +20,15 @@ $header = get_sub_field('header');
                     <div class="col-12 col-sm-6">
                         <div class="col-12 subheading"><h3><?php echo $subheading; ?></h3></div>
                         <div class="col-12 subcontent">
-                            <div class="row"><h5><?php echo $text; ?></h5></div>
-                            <?php //foreach($tags as $tag) : ?>
-                            <!-- <div class="row d-inline-block mb-1 mt-1">
+                            <div class="row pb-3"><h5><?php echo $text; ?></h5></div>
+                            <?php foreach($tags as $tag) : ?>
+                            <div class="row d-inline-block mb-1 mt-1">
                                 <ul>
-                                    <li class="mr-1"><h6><?php //echo $tag['tag']; ?></h6></li>
-                                </ul> -->
-                            <!-- </div> -->
-                        <?php //endforeach; ?>
-                        <?php for($i = 0; $i < 3; $i++) : ?>
-                            <div class="row d-inline-block mb-1 mt-2">
-                                <ul>
-                                    <li class="mr-1"><h6><?php echo $tags[$i]['tag']; ?></h6></li>
+                                    <li class="mr-1"><h6><?php echo $tag['tag']; ?></h6></li>
                                 </ul>
                             </div>
-                        <?php endfor; ?>
+                        <?php endforeach; ?>
+                      
                         </div>
                     </div>
                     <div class="col-12 col-sm-6 background-col" style="background-image: url(<?php echo $image['url']; ?>);"></div>
