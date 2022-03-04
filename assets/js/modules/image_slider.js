a52dc3f121321229a455b80bd58e7bf027d90256
Pernilla Sterner <pernilla@techlove.se>
2022-03-04 13:43:09 +0100

    Fixed cannot read properties of undefined

--- assets/js/modules/image_slider.js
+++ assets/js/modules/image_slider.js
@@ -2,64 +2,56 @@ const IMAGE_SLIDER = (function () {
 
    const scrollAmount = -600;
    const containers   = document.querySelectorAll('#text_image_container');
-   const image = document.querySelector('#image-container');
+   const image_container = document.querySelector('#image-container');
    const bg_images = document.querySelectorAll('#background-img');
    const subheadings = document.querySelectorAll('#text_image_subheading');
   
 
    window.addEventListener('scroll', () => {
   
-       const { top }= image.getBoundingClientRect();
-       const windowTop = top - window.innerHeight;
+      const { top } = image_container.getBoundingClientRect();
+      const windowTop = top - window.innerHeight;
 
-         for (let i = 0; i <= containers.length; i++) {
-            
-            if( windowTop > scrollAmount ) {
-           
-               if( i === 0 ) {
-                  bg_images[i].style.opacity = 1;
-                  subheadings[i].classList.add('is-active');
-                  containers[i].classList.add('is-active');
-               } else {
-                  bg_images[i].style.opacity = 0;
-                  subheadings[i].classList.remove('is-active');
-                  containers[i].classList.remove('is-active');
-               }
-   
-            } else if (windowTop < scrollAmount && windowTop > scrollAmount - 100) {
-   
-               if( i === 1 ) {
-                  bg_images[i].style.opacity = 1;
-                  subheadings[i].classList.add('is-active');
-                  containers[i].classList.add('is-active');
-               } else {
-                  bg_images[i].style.opacity = 0;
-                  subheadings[i].classList.remove('is-active');
-                  containers[i].classList.remove('is-active');
-               }
-   
-            } else if(windowTop < scrollAmount - 100) { 
-   
-               if( i === 2 ) {
-                  bg_images[i].style.opacity = 1;
-                  subheadings[i].classList.add('is-active');
-                  containers[i].classList.add('is-active');
-               } else {
-                  bg_images[i].style.opacity = 0;
-                  subheadings[i].classList.remove('is-active');
-                  containers[i].classList.remove('is-active');
-               }
+      for (let i = 0; i < containers.length; i++) {
+         
+         if( windowTop > scrollAmount ) {
+         
+            if( i === 0 ) {
+               bg_images[i].classList.add('is-active');
+               subheadings[i].classList.add('is-active');
+               containers[i].classList.add('is-active');
+            } else {
+               bg_images[i].classList.remove('is-active');
+               subheadings[i].classList.remove('is-active');
+               containers[i].classList.remove('is-active');
+            }
+
+         } else if (windowTop < scrollAmount && windowTop > scrollAmount - 100) {
+
+            if( i === 1 ) {
+               bg_images[i].classList.add('is-active');
+               subheadings[i].classList.add('is-active');
+               containers[i].classList.add('is-active');
+            } else {
+               bg_images[i].classList.remove('is-active');
+               subheadings[i].classList.remove('is-active');
+               containers[i].classList.remove('is-active');
+            }
+
+         } else if(windowTop < scrollAmount - 100) { 
+
+            if( i === 2 ) {
+               bg_images[i].classList.add('is-active');
+               subheadings[i].classList.add('is-active');
+               containers[i].classList.add('is-active');
+            } else {
+               bg_images[i].classList.remove('is-active');
+               subheadings[i].classList.remove('is-active');
+               containers[i].classList.remove('is-active');
             }
          }
-   
-       //     image1.style.opacity = backgroundInView ? 0 : 1;
-       //     image2.style.opacity = backgroundInView ? 0 : 1;
-       //     image3.style.opacity = backgroundInView ? 1 : 0;
-
-
+      }
    })
-
 })();
-
 
 export { IMAGE_SLIDER }
