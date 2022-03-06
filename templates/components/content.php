b2267cfe112741ba61f6525c92addb57a63a9fd1
Pernilla Sterner <pernilla@techlove.se>
2022-03-06 10:41:05 +0100

    Add terms & policy. Styling

---
+++ templates/components/content.php
@@ -0,0 +1,18 @@
+<?php
+$header = get_sub_field('header');
+$header_tag = get_sub_field('header_tag');
+$content = get_sub_field('content');
+?>
+
+<section class="component component_content">
+
+<div class="container">
+	<?php if($header) : ?>
+		<?php echo '<' . $header_tag . ' class="h1 container-fluid">' . $header . '</' . $header_tag . '>'; ?>
+	<?php endif; ?>
+	<div class="container-fluid editor_content">
+		<?php echo $content; ?>
+	</div>
+    </div>
+
+</section>
