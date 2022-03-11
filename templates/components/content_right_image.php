1fe2b3ce4faa2bd162320d475272d1d2f5ecf91e
Pernilla Sterner <pernilla@techlove.se>
2022-03-11 10:53:01 +0100

    Set image widht max-width

--- templates/components/content_right_image.php
+++ templates/components/content_right_image.php
@@ -11,12 +11,13 @@ $image = get_sub_field('image');
         <div class="col-sm">
             <?php echo '<' . $header_tag . '>' . $header . '</' . $header_tag . '>'; ?>
             <?php echo '<p>' . $text . '</p>'; ?>
-            <img title="qr-image" alt="qr-image" src="<?php echo $qr_image['url']; ?>">
+            <img class="qr-image" title="qr-image" alt="qr-image" src="<?php echo $qr_image['url']; ?>">
+            <button id="modul_open_hero" href="<?php echo $button['link']; ?>" class="btn btn-primary btn-download">download the buildbite app</button>
         </div>
 
 
-        <div class="col-sm">
-            <img title="image" alt="image" src="<?php echo $image['url']; ?>">
+        <div class="col-sm d-flex flex-column-reverse align-items-end">
+            <img title="image" alt="image" class="image" src="<?php echo $image['url']; ?>">
         </div>
     </div>
 </section>
