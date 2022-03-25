0d7ad8ab549dfc85b1d7b96305e5ae8639485c6e
Pernilla Sterner <pernilla@techlove.se>
2022-03-25 09:39:16 +0100

    Display selected tags and move header to top

--- templates/components/parts/desktop.php
+++ templates/components/parts/desktop.php
@@ -1,13 +1,14 @@
 <?php
 $rows = get_sub_field('row');
 $header = get_sub_field('header');
+$total_tags = 8;
 ?>
 
 <section id="why_buildbite" class="component component_text_image desktop">
+<h2><?php echo $header; ?></h2>
     <?php if($rows) : ?>
         <div class="row first no-gutters">
             <div class="col left">
-                <h2><?php echo $header; ?></h2>
                 <?php $y = 0; ?>
                 <?php foreach($rows as $row) : ?>
                 <?php 
@@ -16,23 +17,19 @@ $header = get_sub_field('header');
                 $text = $content['text'];
                 $tags = $content['tags'];
                 ?>
-                <div id="text_image_container" class="container_content text_image_container<?php echo '-' . $y; ?>">
-                    <h3 id="text_image_subheading" class="col-4 text_image_subheading<?php echo '-' . $y; ?>"><?php echo $subheading; ?></h3>
+                <div id="text_image_container" class="container_content">
+                    <h3 id="text_image_subheading" class="col-4"><?php echo $subheading; ?></h3>
                     <div class="col-8 container_text">
-                        <h5><?php echo $text; ?></h5>
-                        <?php foreach($tags as $tag) : ?>
-                            <!-- <div class="row d-inline-block mb-1 mt-1">
-                                <ul>
-                                    <li class="mr-1"><h6><?php //echo $tag['tag']; ?></h6></li>
-                                </ul> -->
-                            <!-- </div> -->
-                        <?php endforeach; ?>
-                        <?php for($i = 0; $i < 3; $i++) : ?>
-                            <div class="row d-inline-block mb-1 mt-2">
-                                <ul>
-                                    <li class="mr-1"><h6><?php echo $tags[$i]['tag']; ?></h6></li>
-                                </ul>
-                            </div>
+                        <h5 class="mb-2"><?php echo $text; ?></h5>
+                        <!-- Only display selected tags -->
+                        <?php for($i = 0; $i <= $total_tags; $i++) : ?>
+                            <?php if( isset($tags[$i]['tag']) ) : ?>
+                                <div class="row d-inline-block mb-1">
+                                    <ul>
+                                        <li class="mr-1"><h6><?php echo $tags[$i]['tag']; ?></h6></li>
+                                    </ul>
+                                </div>
+                            <?php endif; ?>
                         <?php endfor; ?>
                     </div>
                 </div>
@@ -46,12 +43,10 @@ $header = get_sub_field('header');
                 $content = $row['content'];
                 $image = $content['image']; 
                 ?>
-                 <div id="background-img<?php echo '-' . $i; ?>" class="background-col" style="background-image: url(<?php echo $image['url']; ?>);"></div>
+                 <div id="background-img" class="background-col" style="background-image: url(<?php echo $image['url']; ?>);"></div>
                 <?php $i++; ?>
             <?php endforeach; ?>
             </div>
         </div>
     <?php endif; ?>
 </section>
-
-
