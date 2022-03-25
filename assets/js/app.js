89b1e0a7118c7fd4e755e3280f7c62a7a6f774d9
Pernilla Sterner <pernilla@techlove.se>
2022-03-25 15:48:50 +0100

    Remove toggle functions for feature and get started

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,27 +8,6 @@ const APP = (function () {
     document.addEventListener('DOMContentLoaded', () => {
 
         MENU.addListeners();
-
-        const open = document.querySelectorAll('.open');
-        const close = document.querySelectorAll('.close');
-        const modul_get_started = document.getElementById('modul_get-started');
-
-        const openFeatures = document.querySelectorAll('.open-features');
-        const modul_features = document.getElementById('modul_features');
-        
-        openFeatures.forEach(el => el.addEventListener('click', () => {
-            modul_features.classList.add('modul_show')
-        }));
-
-        open.forEach(el => el.addEventListener('click', () => {
-            modul_get_started.classList.add('modul_show')
-        }));
-
-        close.forEach(el => el.addEventListener('click', () => {
-
-            modul_get_started.classList.remove('module_show')
-            modul_features.classList.remove('modul_show')
-        }));
-        
+    
     })
 })();
